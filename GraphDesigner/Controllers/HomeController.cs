b02f640497c77de66e69191989efa93259426eef
Nahum Fabian <fabiann@surgent.com>
2021-05-28 22:18:38 -0600

    remove unneeded html

--- GraphDesigner/Controllers/HomeController.cs
+++ GraphDesigner/Controllers/HomeController.cs
@@ -68,11 +68,6 @@ namespace GraphDesignerApi.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
